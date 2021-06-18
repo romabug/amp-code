54c6a47a126a4546b60403d0b608dc36823656cb
Hanchang cai <hanchangcai@gmail.com>
2021-06-18 13:07:03 +1000

    add interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,34 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 export class AppComponent implements OnInit {
 
-  public inputValue1:any  = {
+  public inputValue1: any = {
     'inputText': '55.55555%',
     'decimalPlaces': '1'
   }
 
-  public inputValue2:any  = {
+  public inputValue2: any = {
     'inputText': '0.65555',
     'decimalPlaces': '2'
   }
 
-  public inputValue3:any  = {
+  public inputValue3: any = {
     'inputText': 0.885555,
     'decimalPlaces': '3'
   }
 
+  public inputValue4: any = {
+    'inputText': 0.885555,
+    'decimalPlaces': 2
+  }
+
+
   ngOnInit() {
+    // comment out this setInterval to remove continual input.
+    setInterval(() => {
+      this.inputValue4.inputText -= 0.001;
+      this.inputValue4.decimalPlaces = Math.floor(Math.random() * 4);
+    }, 1000)
+
   }
 
 
